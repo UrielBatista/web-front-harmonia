e16a7d4ceb835b0b3b2e3ebe9b68f102de28f53b
UrielBatista <ur.sabatista@gmail.com>
2023-11-26 17:19:00 -0300

    Adjustment url

--- web-front/src/services/api.js
+++ web-front/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     
-    baseURL: process.env.REACT_APP_URL_BASE, //BASE URL DO BACKEND
+    baseURL: 'http://34.125.24.146:8090/api/', //BASE URL DO BACKEND
     headers: {
         'Content-Type': 'application/json',
     }   
